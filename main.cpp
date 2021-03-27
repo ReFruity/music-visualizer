67ec047e7cb34ffa3c842808b811ae27fca85919
Anton Istomin <Anton.Istomin@ibm.com>
2021-03-27 17:57:09 +0300

    OpenCV hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "opencv2/opencv.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/types_c.h>
 
 using namespace cv;
 
@@ -13,13 +14,13 @@ int main() {
     Mat output = Mat::zeros( 120, 350, CV_8UC3 );
 
     //write text on the matrix:
-    // putText(output,
-    //         "Hello World :)",
-    //         cvPoint(15,70),
-    //         FONT_HERSHEY_PLAIN,
-    //         3,
-    //         cvScalar(0,255,0),
-    //         4);
+    putText(output,
+            "Hello World :)",
+            cvPoint(15,70),
+            FONT_HERSHEY_PLAIN,
+            3,
+            cvScalar(0,255,0),
+            4);
 
     //display the image:
     imshow("Output", output);
