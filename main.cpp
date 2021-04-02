1bf8a03164c383f7f881145d4f8b2ba964c14cb8
Anton Istomin <Anton.Istomin@ibm.com>
2021-04-03 02:03:28 +0300

    Print input audio stream

--- main.cpp
+++ main.cpp
@@ -4,10 +4,73 @@
 #include <opencv2/core/types_c.h>
 #include <portaudio.h>
 
+typedef struct
+{
+    float left_phase;
+    float right_phase;
+}
+paData;
+
+static paData *data1;
+
+static int paCallback(const void *inputBuffer, void *outputBuffer,
+                      unsigned long framesPerBuffer,
+                      const PaStreamCallbackTimeInfo* timeInfo,
+                      PaStreamCallbackFlags statusFlags,
+                      void *userData )
+{
+    auto inputBufferFloat32 = (const float *) inputBuffer;
+
+    // std::cout << "-----" << std::endl;
+    // std::cout << framesPerBuffer << std::endl;
+    // std::cout << sizeof(*inputBufferFloat32) << std::endl;
+    // std::cout << sizeof(inputBufferFloat32[0]) << std::endl;
+    // std::cout << inputBufferFloat32[0] << std::endl;
+
+    if (inputBuffer == nullptr) std::cout << "Input buffer is nullptr, listen to the sound of silence" << std::endl;
+
+    for (int i = 0; i < framesPerBuffer; i++) {
+        std::cout << *inputBufferFloat32++ << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
+
 int main() {
     auto err = Pa_Initialize();
 
-    std::cout << err << std::endl;
+    std::cout << Pa_GetErrorText(err) << std::endl;
+
+    int numDevices = Pa_GetDeviceCount();
+    if (numDevices < 0) {
+        printf( "ERROR: Pa_CountDevices returned 0x%x\n", numDevices );
+    }
+
+    const PaDeviceInfo *deviceInfo;
+    for (int i=0; i < numDevices; i++)  {
+        deviceInfo = Pa_GetDeviceInfo( i );
+        std::cout << i << " " << deviceInfo->name << " " << deviceInfo->defaultSampleRate << " "
+                  << deviceInfo->maxInputChannels << " " << deviceInfo->maxOutputChannels << std::endl;
+    }
+
+    PaStream *stream;
+    PaStreamParameters inputParameters;
+    bzero(&inputParameters, sizeof(inputParameters));
+    inputParameters.channelCount = 1;
+    inputParameters.device = 1;
+    inputParameters.sampleFormat = paFloat32;
+    double sampleRate = 44100;
+    unsigned long framesPerBuffer = paFramesPerBufferUnspecified;
+    err = Pa_OpenStream(&stream, &inputParameters, nullptr, sampleRate, framesPerBuffer, paNoFlag,
+                        paCallback, &data1);
+
+    std::cout << Pa_GetErrorText(err) << std::endl;
+
+    err = Pa_StartStream(stream);
+
+    std::cout << Pa_GetErrorText(err) << std::endl;
 
     cv::namedWindow("Output",1);
     cv::Mat output = cv::Mat::zeros( 120, 350, CV_8UC3 );
@@ -24,8 +87,7 @@ int main() {
     cv::waitKey(0);
 
     err = Pa_Terminate();
-    std::cout << err << std::endl;
+    std::cout << Pa_GetErrorText(err) << std::endl;
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
